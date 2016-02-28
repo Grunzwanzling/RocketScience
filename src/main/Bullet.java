861a926bedc91ae3c4691656832afee26c2c8159
Grunzwanzling <maximilian@gaisberg-helfenberg.de>
2016-02-28 22:36:29 +0100

    Added settings for Bullets

--- src/main/Bullet.java
+++ src/main/Bullet.java
@@ -14,13 +14,13 @@ public class Bullet implements UpdateListener {
 	double x, y;
 	Color color;
 	double speedX, speedY;
-	int bouncesLeft, timeLeft;
+	int bouncesLeft, timeLeft, size;
 	boolean willExplode;
 	boolean tracker;
 
 	public Bullet(double x, double y, double speedX, double speedY,
-			Color color, int bouncesLeft, int timeLeft, boolean willExplode,
-			boolean tracker) {
+			Color color, int bouncesLeft, int timeLeft, int size,
+			boolean willExplode, boolean tracker) {
 		this.x = x;
 		this.y = y;
 		this.speedX = speedX;
@@ -28,6 +28,86 @@ public class Bullet implements UpdateListener {
 		this.color = color;
 		this.bouncesLeft = bouncesLeft;
 		this.timeLeft = timeLeft;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public double getSpeedX() {
+		return speedX;
+	}
+
+	public void setSpeedX(double speedX) {
+		this.speedX = speedX;
+	}
+
+	public double getSpeedY() {
+		return speedY;
+	}
+
+	public void setSpeedY(double speedY) {
+		this.speedY = speedY;
+	}
+
+	public int getBouncesLeft() {
+		return bouncesLeft;
+	}
+
+	public void setBouncesLeft(int bouncesLeft) {
+		this.bouncesLeft = bouncesLeft;
+	}
+
+	public int getTimeLeft() {
+		return timeLeft;
+	}
+
+	public void setTimeLeft(int timeLeft) {
+		this.timeLeft = timeLeft;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public boolean isWillExplode() {
+		return willExplode;
+	}
+
+	public void setWillExplode(boolean willExplode) {
+		this.willExplode = willExplode;
+	}
+
+	public boolean isTracker() {
+		return tracker;
+	}
+
+	public void setTracker(boolean tracker) {
+		this.tracker = tracker;
 	}
 
 	@Override
